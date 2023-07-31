f4d937fb4a4d970cfbdea6c563947a47f5009a82
Tarun Patil <tarunpatil001@gmail.com>
2023-07-31 08:53:24 +0530

    13. Array -->> 6. Largest number in Array.mp4 added.

--- array.java
+++ array.java
@@ -2,47 +2,25 @@ import java.util.*;
 
 public class array {
 
-    public static int LinearSearch(int numbers[], int key) {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
 
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == key) {
-                return i;
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
             }
         }
-
-        return -1;
-    }
-
-    public static String LinearMenu(String menu[], String key) {
-
-        for (int i = 0; i < menu.length; i++) {
-            if (menu[i] == key) {
-                return String.valueOf(i);
-            }
-        }
-        return String.valueOf(-1);
+        System.out.println("Smallest No.: " + smallest);
+        return largest;
     }
 
     public static void main(String args[]) {
-        int numbers[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
-        int key = 20;
-
-        int index = LinearSearch(numbers, key);
-        if (index == -1) {
-            System.out.println("Key NOT found");
-        } else {
-            System.out.println("Key found at index: " + index);
-        }
-
-        String menu[] = { "Dosa", "Kachori", "Samosa", "Fafda" };
-        String key = "Samosa";
-
-        String index = LinearMenu(menu, key);
-        if (index.equals(String.valueOf(-1))) {
-            System.out.println("Menu NOT found.");
-        } else {
-            System.out.println("Menu found at index: " + index);
-        }
+        int numbers[] = { 7, 9, 6, 3, 5 };
+        System.out.println("Largest No.: " + getLargest(numbers));
 
     }
 }
