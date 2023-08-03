242ed6e7dab0844966ac500d6596b1ce28e67494
Tarun Patil <tarunpatil001@gmail.com>
2023-08-03 18:24:30 +0530

    Print SubArray program created.

--- printSubArrays.java
+++ printSubArrays.java
@@ -1,3 +1,19 @@
 public class printSubArrays {
 
+    public static void subArrays(int numbers[]) {
+        for (int row = 0; row < numbers.length; row++) {
+            for (int col = row; col <= numbers.length; col++) {
+                for (int k = row; k < col; k++) {
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        subArrays(numbers);
+    }
 }
