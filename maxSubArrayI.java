145064a3b9a72a60e82b671213d6fc8c68f3be86
Tarun Patil <tarunpatil001@gmail.com>
2023-08-07 11:52:54 +0530

    Max sub array using prefix array program created.

--- maxSubArrayI.java
+++ maxSubArrayI.java
@@ -3,28 +3,32 @@ public class maxSubArrayI {
     public static void maxSubArraySum(int numbers[]) {
         int currentSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+        // Calculate prefix array:
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
 
         for (int row = 0; row < numbers.length; row++) {
+            int start = row;
 
             for (int col = row; col < numbers.length; col++) {
+                int end = col;
+                currentSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
 
-                currentSum = 0;
-                for (int k = row; k <= col; k++) {
-                    currentSum += numbers[k];
-                }
-                System.out.print(currentSum + "\t");
                 if (maxSum < currentSum) {
                     maxSum = currentSum;
                 }
             }
-            System.out.println();
         }
 
         System.out.println("Max sum: " + maxSum);
     }
 
     public static void main(String args[]) {
-        int numbers[] = { 1, -2, 6, -1, 3 };
+        int numbers[] = { 2, 4, 6, 8, 10 };
         maxSubArraySum(numbers);
     }
 }
