ebfd1c87d0f4ec992573d4caf08d591bdef10d92
Tarun Patil <tarunpatil001@gmail.com>
2023-08-12 09:56:16 +0530

    Selection Sort program added

---
+++ selectionSorting.java
@@ -0,0 +1,30 @@
+
+public class selectionSorting {
+
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minpos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minpos] > arr[j]) {
+                    minpos = j;
+                }
+            }
+
+            int temp = arr[minpos];
+            arr[minpos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printSelectionSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 2, 1, 4, 3 };
+        selectionSort(arr);
+        printSelectionSort(arr);
+    }
+}
