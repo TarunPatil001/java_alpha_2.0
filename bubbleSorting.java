218f7af03b1b96b88337882b430d6dea37098715
Tarun Patil <tarunpatil001@gmail.com>
2023-08-12 09:21:45 +0530

    Bubble Sort program added

---
+++ bubbleSorting.java
@@ -0,0 +1,27 @@
+
+public class bubbleSorting {
+
+    public static void bubbleSortAlgo(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printBubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 1, 2, 4, 3 };
+        bubbleSortAlgo(arr);
+        printBubbleSort(arr);
+    }
+}
